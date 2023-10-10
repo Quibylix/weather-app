fbe27fb6e292a03339e8b688c92fdbc61e8e816b
Vanaldito <barraza.fredimanuel@gmail.com>
2023-10-10 14:52:36 -0600

    feat: create the humidity, pressure and wind icon components

---
+++ components/Icons/index.ts
@@ -0,0 +1,3 @@
+export { default as HumidityIcon } from "./HumidityIcon";
+export { default as PressureIcon } from "./PressureIcon";
+export { default as WindIcon } from "./WindIcon";
--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
+export * from "./Icons";
 export { default as WeatherIcon } from "./WeatherIcon";
