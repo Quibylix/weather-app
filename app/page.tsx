262184a5b26f651682ec2a7a81d27634f65ea7a5
Vanaldito <barraza.fredimanuel@gmail.com>
2023-10-10 14:50:25 -0600

    feat: display the weather, temperature, the wind speed, the pressure and the humidity

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import { WeatherIcon } from "@/components";
 import { getWeatherFromCoords } from "@/services";
+import styles from "./page.module.css";
 
 export default async function HomePage() {
   const res = await getWeatherFromCoords(33.44, -94.04);
 
   return (
-    <main>
-      <h1>Weather App</h1>
-      <p>{res.weather.main}</p>
-      <WeatherIcon icon={res.weather.icon} />
+    <main className={styles.page}>
+      <WeatherIcon className={styles.icon} icon={res.weather.icon} />
+      <h2 className={styles.temperature}>
+        <span className={styles.temperatureValue}>
+          {(res.temperature - 273.15).toFixed(0)}
+        </span>
+        <span className={styles.temperatureUnits}>°C</span>
+      </h2>
+      <h3 className={styles.weather}>{res.weather.main}</h3>
+      <div className={styles.details}>
+        <div className={styles.detail}>Humidity: {res.humidity}%</div>
+        <div className={styles.detail}>Pressure: {res.pressure} hPa</div>
+        <div className={styles.detail}>
+          Wind speed: {res.windSpeed.toFixed(2)} m/s
+        </div>
+      </div>
     </main>
   );
 }
--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,47 @@
+import { weatherConditionCodes } from "@/constants";
+
 describe("general app test", () => {
   it("renders the title 'Weather App'", () => {
     cy.visit("http://localhost:3000");
 
     cy.title().should("include", "Weather App");
+  });
+
+  it("renders an icon with the alt text 'Weather Icon'", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.get("img").should("have.attr", "alt", "Weather Icon");
+  });
+
+  it("renders the current weather for the default location", () => {
+    cy.visit("http://localhost:3000");
+
+    const possibleWeather = [...Object.keys(weatherConditionCodes)];
+
+    cy.contains(new RegExp(possibleWeather.join("|"), "i"));
+  });
+
+  it("renders the current temperature for the default location", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.contains(/\d+°C/);
+  });
+
+  it("renders the current pressure for the default location", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.contains(/Pressure: \d+ hPa/);
+  });
+
+  it("renders the current humidity for the default location", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.contains(/Humidity: \d+%/);
+  });
+
+  it("renders the current wind speed for the default location", () => {
+    cy.visit("http://localhost:3000");
+
+    cy.contains(/Wind speed: \d+\.\d{2} m\/s/);
   });
 });
