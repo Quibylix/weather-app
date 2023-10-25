6722a910a2222f2d1f8b845460ef59eec85f3b71
Vanaldito <barraza.fredimanuel@gmail.com>
2023-10-25 09:42:06 -0600

    feat: create the SearchIcon component

--- components/Icons/index.ts
+++ components/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as HumidityIcon } from "./HumidityIcon";
 export { default as PressureIcon } from "./PressureIcon";
+export { default as SearchIcon } from "./SearchIcon";
 export { default as WindIcon } from "./WindIcon";
